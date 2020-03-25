4d1602618ac87d21fa8e290b413ad90f8dd3866a
zhaonan <zhaonan420@qq.com>
2020-03-25 16:43:12 +0800

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Auth::routes(['verify' => true]);
 Route::redirect('/', '/products')->name('root');
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
+    //用户地址
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
@@ -23,10 +24,15 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 
+    //商品管理
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
+    //购物车管理
+    Route::get('cart', 'CartController@index')->name('cart.index');
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
 
 Route::get('products', 'ProductsController@index')->name('products.index');
