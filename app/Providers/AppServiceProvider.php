3701e26cecc0c4bffa0702cd82c570bd842fc916
zhaonan <zhaonan420@qq.com>
2020-04-02 17:05:41 +0800

    引入支付库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,31 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
+
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+            // 判断当前项目运行环境是否为线上环境
+            if (app()->environment() !== 'production') {
+                $config['mode']         = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个支付宝支付对象
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个微信支付对象
+            return Pay::wechat($config);
+        });
+
 
     }
 
