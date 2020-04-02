33a0cb2b4c85349846dc5c44bcd6f90bb3eed94b
zhaonan <zhaonan420@qq.com>
2020-04-02 17:57:31 +0800

    test

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\InvalidRequestException;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    public function payByAlipay(Order $order, Request $request)
+    {
+        $this->authorize('own', $order);
+
+        if ($order->paid_at || $order->closed) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+
+        return app('alipay')->web([
+            'out_trade_no' => $order->no, // 订单编号，需保证在商户端不重复
+            'total_amount' => $order->total_amount, // 订单金额，单位元，支持小数点后两位
+            'subject'      => '支付 Laravel Shop 的订单：'.$order->no, // 订单标题
+        ]);
+    }
+}
