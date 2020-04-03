c368157bee102fec67ce61ccb20c2b4407c33b11
zhaonan <zhaonan420@qq.com>
2020-04-03 14:41:25 +0800

    订单支付

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
@@ -19,7 +20,43 @@ class PaymentController extends Controller
         return app('alipay')->web([
             'out_trade_no' => $order->no, // 订单编号，需保证在商户端不重复
             'total_amount' => $order->total_amount, // 订单金额，单位元，支持小数点后两位
-            'subject'      => '支付 Laravel Shop 的订单：'.$order->no, // 订单标题
+            'subject' => '支付 Laravel Shop 的订单：' . $order->no, // 订单标题
         ]);
+    }
+
+
+    // 前端回调页面
+    public function alipayReturn()
+    {
+        // 校验提交的参数是否合法
+        $data = app('alipay')->verify();
+        dd($data);
+    }
+
+
+    // 服务器端回调
+    public function alipayNotify()
+    {
+        // 校验输入参数
+        $data = app('alipay')->verify();
+        // $data->out_trade_no 拿到订单流水号，并在数据库中查询
+        $order = Order::where('no', $data->out_trade_no)->first();
+        // 正常来说不太可能出现支付了一笔不存在的订单，这个判断只是加强系统健壮性。
+        if (! $order) {
+            return 'fail';
+        }
+        // 如果这笔订单的状态已经是已支付
+        if ($order->paid_at) {
+            // 返回数据给支付宝
+            return app('alipay')->success();
+        }
+
+        $order->update([
+            'paid_at' => Carbon::now(), // 支付时间
+            'payment_method' => 'alipay', // 支付方式
+            'payment_no' => $data->trade_no, // 支付宝订单号
+        ]);
+
+        return app('alipay')->success();
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
+            $config['notify_url'] = route('payment.alipay.notify');
+            $config['return_url'] = route('payment.alipay.return');
+
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
                 $config['mode']         = 'dev';
