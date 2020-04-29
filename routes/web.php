8888cfe3d1d600c37700c1da9ddbddc756b994b7
zhaonan <zhaonan420@qq.com>
2020-04-29 11:50:15 +0800

    初始化类目模型

--- routes/web.php
+++ routes/web.php
@@ -57,4 +57,11 @@ Route::get('products/{product}', 'ProductsController@show')->name('products.show
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 
 
+Route::get('alipay', function() {
+    return app('alipay')->web([
+        'out_trade_no' => time(),
+        'total_amount' => '1',
+        'subject' => 'test subject - 测试',
+    ]);
+});
 
